2efa6b7923c31a9b7287560bc491bf4ce0dd68c9
Patrick Samuel <patrick-samuel@live.com>
2025-04-09 23:08:31 +0100

    exercise 1 and 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(inputText,repetitionMax) {
+    let resultString = [];
+    if (repetitionMax<0){
+        return "ERROR"
+    }
+    for (let i = 0; i<repetitionMax; i++) {
+        resultString.push(inputText);
+    }
+    return resultString.join("")
 };
 
 // Do not edit below this line
