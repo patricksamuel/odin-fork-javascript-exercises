3fda9855ef3cd73c46c61fcc6d21898a9d3aa86a
Patrick Samuel <patrick-samuel@live.com>
2025-04-10 11:59:46 +0100

    3rd execercise done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArrays, ...toBeRemoved) {
+    let argumentArray=Array.from(inputArrays);
+    let outputArray =[];
+    for (const arg of argumentArray){
+        outputArray.push(arg);
+        for (const elementtoBeRemoved of toBeRemoved) {
+            if (arg === elementtoBeRemoved ) {
+                outputArray.pop();
+            }
+        }
+
+    }
+    return outputArray;
 };
 
 // Do not edit below this line
